81d96c04ab45fa52955fa7aca1cff05a09c88855
Alisa <allisatumanova@gmail.com>
2023-05-19 16:00:42 +0300

    Подготовлены юнит тесты для проверки работы функций

--- BottleWebProject_C022_4_TGKS/logic/TumFunctions.py
+++ BottleWebProject_C022_4_TGKS/logic/TumFunctions.py
@@ -1,6 +1,5 @@
 import random
 
-@post('/bin_relation_equality', method='relations')
 def Matrix(num_vert, matrixD):
     
     matrix = [[0] * n for i in range(n)]
@@ -16,16 +15,19 @@ def Matrix(num_vert, matrixD):
     for i in range(n):
         for i2 in range(n):
                 if (matrix[i][i2] == 1 and i == i2):
-                   a+=1
+                   a += 1
                 elif (matrix[i][i2] == 1 and matrix[i][i2] == matrix[i2][i]):
                     if (matrix[i][i2] == 1):
                         for i3 in range(n):
-                            if (matrix[i2][i3] == 1 and matrix[i3][i] == 1 and i2!=i3):
+                            if (matrix[i2][i3] == 1 and matrix[i3][i] == 1 and i2 != i3):
                                a += 1
-                elif matrix[i][i2]==0: a+=1
+                elif matrix[i][i2] == 0: a += 1
                 else: break
-    if a > (n*n): res = "Заданное бинарное отношение является эквивалентным."
+    if a >= (n * n): res = "Заданное бинарное отношение является эквивалентным."
     else: res = "Заданное бинарное отношение не является эквивалентным."
+
+    #if a >= (n * n): return True
+    #else: return False
 
     for i in range(n):
         for i2 in range(n):
