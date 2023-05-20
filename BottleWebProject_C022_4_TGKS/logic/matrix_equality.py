0b771ec464cbbbdc3d98670a8c9d51a457ce6e5f
Alisa <allisatumanova@gmail.com>
2023-05-20 15:20:21 +0300

    Добавлен рандом

--- BottleWebProject_C022_4_TGKS/logic/matrix_equality.py
+++ BottleWebProject_C022_4_TGKS/logic/matrix_equality.py
@@ -1,4 +1,5 @@
 from bottle import route, request, template, view, datetime
+import random
 from logic.function_of_equality import function_of_equality
 
 @route('/bin_relation_equality', method='POST')
@@ -11,8 +12,14 @@ def submit():
             cell = request.forms.get('matrix[{}][{}]'.format(i, j))
             row.append(int(cell) if cell else 0)
         matrix.append(row)
-    #вызов функции с результатом
+    #создание экземпляра класса
     enterMatrix = function_of_equality(num_vertices, matrix)
+
+    if 'randomGenerate' in request.forms:
+        #если нажата кнопка сгенерировать и решить задачу генирация матрицы происходит рандомно
+        for i in range(num_vertices):
+            for i2 in range(num_vertices):
+                    enterMatrix.matrix[i][i2] = random.randit(0, 1)
     is_equality = enterMatrix.is_equality_matrix() + " Ниже преведено дополнительное отншоение заданного: "
     dop_matrix = enterMatrix.dop_relation()
 
