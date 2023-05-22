1b73c748abea401454ada1873e33d50938066610
Alisa <allisatumanova@gmail.com>
2023-05-22 15:57:04 +0300

    Доделаны изменения в юниттестах

--- BottleWebProject_C022_4_TGKS/unitTests/test_tum_unittest.py
+++ BottleWebProject_C022_4_TGKS/unitTests/test_tum_unittest.py
@@ -1,8 +1,9 @@
 import unittest
+import sys
 from logic.function_of_equality import function_of_equality
 
 class test_function_of_equality_check_tests(unittest.TestCase):
-    def test_function_of_equality_matrix_are_equality(self):
+    def test_is_equality_matrix_matrix_are_equality(self):
         #arrange
         vert = 6
         matrix = [[1, 0, 0, 0, 0, 1], [0, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 0], [1, 0, 0, 0, 0, 1]]
@@ -10,28 +11,105 @@ class test_function_of_equality_check_tests(unittest.TestCase):
 
         #act
         example = function_of_equality(vert, matrix)
-        result = example.Matrix()
+        result = example.is_equality_matrix()
 
         #assert
         self.assertEqual(result, expected)
 
-    def test_function_of_equality_matrix_are_not_equality(self):
+
+    def test_is_equality_matrix_big_matrix_are_equality(self):
         #arrange
         vert = 10
-        matrix = [[1, 0, 0, 0, 0, 1, 0, 1, 1, 0], [0, 1, 1, 1, 0, 1, 0, 1, 1, 0], [0, 1, 1, 0, 0, 1, 0, 1, 1, 0],
-                                  [0, 0, 0, 1, 0, 1, 0, 1, 1, 0], [0, 0, 0, 1, 1, 0, 0, 1, 1, 0], [0, 0, 0, 1, 1, 0, 0, 1, 1, 0],
-                                  [0, 0, 0, 1, 0, 1, 0, 1, 1, 0], [0, 0, 0, 1, 1, 0, 0, 1, 1, 0], [0, 0, 0, 1, 1, 0, 0, 1, 1, 0],
-                                  [1, 0, 0, 0, 0, 1, 0, 1, 1, 0]]
+        matrix = [[1, 0, 0, 0, 0, 1, 0, 1, 1, 1], [0, 1, 1, 1, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                 [0, 1, 1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+                 [0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [1, 0, 0, 0, 0, 1, 0, 1, 1, 1], [1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+                 [1, 0, 0, 0, 0, 1, 0, 1, 1, 1]]
+        expected = "Заданное бинарное отношение является эквивалентным."
+
+        #act
+        example = function_of_equality(vert, matrix)
+        result = example.is_equality_matrix()
+
+        #assert
+        self.assertEqual(result, expected)
+
+
+    def test_is_equality_matrix_little_matrix_are_equality(self):
+        #arrange
+        vert = 4
+        matrix = [[1, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 1]]
+        expected = "Заданное бинарное отношение является эквивалентным."
+
+        #act
+        example = function_of_equality(vert, matrix)
+        result = example.is_equality_matrix()
+
+        #assert
+        self.assertEqual(result, expected)
+
+
+    def test_is_equality_matrix_little_matrix_are_not_equality(self):
+        #arrange
+        vert = 3
+        matrix = [[1, 0, 0], [1, 1, 1], [0, 1, 1]]
         expected = "Заданное бинарное отношение не является эквивалентным."
 
         #act
         example = function_of_equality(vert, matrix)
-        result = example.Matrix()
+        result = example.is_equality_matrix()
 
         #assert
         self.assertEqual(result, expected)
 
-    def test_function_of_equality_Dop_Relation_right(self):
+
+    def test_is_equality_matrix_refleks_matrix_are_not_equality(self):
+        #arrange
+        vert = 10
+        matrix = [[1, 0, 0, 0, 0, 1, 0, 1, 1, 0], [0, 1, 1, 1, 0, 1, 0, 1, 1, 0], [0, 1, 1, 0, 0, 1, 0, 1, 1, 0],
+                 [0, 0, 0, 1, 0, 1, 0, 1, 1, 0], [0, 0, 0, 1, 1, 0, 0, 1, 1, 0], [0, 0, 0, 1, 1, 1, 0, 1, 1, 0],
+                 [0, 0, 0, 1, 0, 1, 1, 1, 1, 0], [0, 0, 0, 1, 1, 0, 0, 1, 1, 0], [0, 0, 0, 1, 1, 0, 0, 1, 1, 0],
+                 [1, 0, 0, 0, 0, 1, 0, 1, 1, 1]]
+        expected = "Заданное бинарное отношение не является эквивалентным."
+
+        #act
+        example = function_of_equality(vert, matrix)
+        result = example.is_equality_matrix()
+
+        #assert
+        self.assertEqual(result, expected)
+
+
+    def test_is_equality_matrix_matrix_are_not_equality(self):
+        #arrange
+        vert = 5
+        matrix = [[1, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 1, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 1, 1]]
+        expected = "Заданное бинарное отношение не является эквивалентным."
+
+        #act
+        example = function_of_equality(vert, matrix)
+        result = example.is_equality_matrix()
+
+        #assert
+        self.assertEqual(result, expected)
+
+
+    def test_Dop_Relation_equality_matrix_right_equality_matrix(self):
+        #arrange
+        vert = 8
+        matrix = [[1, 0, 0, 0, 0, 1, 0, 1], [0, 1, 1, 1, 0, 1, 0, 1], [0, 1, 1, 0, 0, 1, 0, 1], [0, 0, 0, 1, 0, 1, 0, 1],
+                 [0, 0, 0, 1, 1, 0, 0, 1], [0, 0, 0, 1, 1, 0, 0, 1], [0, 0, 0, 1, 1, 0, 0, 1], [0, 0, 0, 1, 1, 0, 0, 1]]
+        expected = [[0, 1, 1, 1, 1, 0, 1, 0], [1, 0, 0, 0, 1, 0, 1, 0], [1, 0, 0, 0, 1, 0, 1, 0], [1, 1, 1, 0, 1, 0, 1, 0],
+                   [1, 1, 1, 0, 0, 1, 1, 0], [1, 1, 1, 0, 0, 1, 1, 0], [1, 1, 1, 0, 0, 1, 1, 0], [1, 1, 1, 0, 0, 1, 1, 0]]
+
+        #act
+        example = function_of_equality(vert, matrix)
+        result = example.Dop_Relation()
+
+        #assert
+        self.assertEqual(result, expected)
+
+
+    def test_Dop_Relation__notequality_matrix_right_not_equality_matrix(self):
         #arrange
         vert = 3
         matrix = [[0, 1, 0], [1, 0, 0], [1, 1, 0]]
@@ -43,3 +121,7 @@ class test_function_of_equality_check_tests(unittest.TestCase):
 
         #assert
         self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
